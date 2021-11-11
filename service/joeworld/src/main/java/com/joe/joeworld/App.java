2f455ca4ede7db13c001397dccf0d2291e70bc5f
SuXiaoo-qiu <87381442@qq.com>
2021-11-11 14:19:55 +0800

    优化启动类

--- service/joeworld/src/main/java/com/joe/joeworld/App.java
+++ service/joeworld/src/main/java/com/joe/joeworld/App.java
@@ -1,6 +1,5 @@
 package com.joe.joeworld;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,10 +8,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @ComponentScan(basePackages = {"com.joe"})
 @SpringBootApplication
-public class JoeworldApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(JoeworldApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }
