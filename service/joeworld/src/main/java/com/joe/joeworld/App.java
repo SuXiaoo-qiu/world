48c45c38f4a8ace5f113f786a2688ce3f6a15ab2
SuXiaoo-qiu <87381442@qq.com>
2021-11-17 10:28:11 +0800

    打印错误日志(之前日志不打印错误日志)

--- service/joeworld/src/main/java/com/joe/joeworld/App.java
+++ service/joeworld/src/main/java/com/joe/joeworld/App.java
@@ -1,13 +1,23 @@
 package com.joe.joeworld;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
+@EnableAutoConfiguration(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 @EnableTransactionManagement
-@ComponentScan(basePackages = {"com.joe"})
+@ServletComponentScan
+@MapperScan("com.joe.**.dao")
 @SpringBootApplication
+@EnableCaching
+@EnableAsync
 public class App {
 
     public static void main(String[] args) {
