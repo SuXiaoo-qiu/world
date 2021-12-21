07c59ed9cf8491cef428a7c06056515e5c3942c4
SuXiaoo-qiu <87381442@qq.com>
2021-12-21 17:39:36 +0800

    oss pom

---
+++ service/oss/src/main/java/com/joe/oss/OssApplication.java
@@ -0,0 +1,19 @@
+package com.joe.oss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class
+})
+@ComponentScan(basePackages = {"com.joe"})
+public class OssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OssApplication.class, args);
+    }
+
+}
